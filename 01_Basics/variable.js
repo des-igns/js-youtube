c006c7c3a87c68414f8689395f567a79e7183c28
Devesh Mathur <78540135+des-igns@users.noreply.github.com>
2023-11-25 21:41:06 +0000

    variables

---
+++ 01_Basics/variable.js
@@ -0,0 +1,10 @@
+console.log("dev");
+
+//number -> 2 to power 53
+//bigint
+//boolean -> true/false
+//string -> ""
+//null => stand alone value
+//undefined
+//object
+//symbol => Unique identifier for components(used in React)
