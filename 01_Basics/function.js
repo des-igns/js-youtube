3e6e0b38b094bf6534ac09d7b6e9c88775b7ee7a
Devesh Mathur <78540135+des-igns@users.noreply.github.com>
2023-11-26 20:14:42 +0000

    function

---
+++ 01_Basics/function.js
@@ -0,0 +1,30 @@
+// function sayname(){
+//     console.log("Devesh")
+//     console.log("Mathur")
+// }
+// sayname();
+
+// function test(a,b){
+//     // let result = a + b
+//     // return result
+//     return a + b
+// }
+// const result = test(5,7)
+// console.log(result);
+
+// function handleObj(anyObject){
+//     console.log(`Username is ${anyObject.username} whose id is ${anyObject.id}`);
+// }
+
+// handleObj({
+//     username: "dev",
+//     id: 123
+// })
+
+const myNewArray = [1,2,3,4,5]
+
+function abc(getArray){
+    return getArray[1]
+}
+
+console.log(abc(myNewArray));
