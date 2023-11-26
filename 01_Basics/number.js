174a9f7950bd7d7cf01afe9708e77ba379756c4a
Devesh Mathur <78540135+des-igns@users.noreply.github.com>
2023-11-26 11:38:47 +0000

    no.

---
+++ 01_Basics/number.js
@@ -0,0 +1,14 @@
+const score = 500;
+//console.log(score)
+
+const balance = new Number(345)
+// console.log(balance)
+// console.log(balance.toString().length)
+//console.log(balance.toFixed(2)) //decimal points
+
+// const a = 4546.454
+// console.log(a.toPrecision(4))
+
+// console.log(Math.random());
+// console.log(Math.random()*100 + 1);
+console.log(Math.floor(Math.random()*100) + 1);
