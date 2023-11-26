c4d25b4bebb2f8606d01fa5e5efbadeb74d7cc33
Devesh Mathur <78540135+des-igns@users.noreply.github.com>
2023-11-26 07:20:59 +0000

    datatypes

--- 01_Basics/variable.js
+++ 01_Basics/variable.js
@@ -1,10 +1,28 @@
-console.log("dev");
+//console.log("dev");
 
+// #Primitive Datatypes
 //number -> 2 to power 53
 //bigint
 //boolean -> true/false
 //string -> ""
 //null => stand alone value
 //undefined
-//object
 //symbol => Unique identifier for components(used in React)
+ //const id = Symbol('123') 
+
+// #Reference(Non primitive) Datatypes
+//object
+// let myObj = {
+//     name: 'Dev',
+//     age: 23,
+// }
+
+//arrays
+//const heroes = ['ironman', 'Spiderman', 'Loki'];
+
+//functions
+// const abc = function(){
+//     console.log("dev");
+
+// }
+// abc();
