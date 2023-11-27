f5a7847749657cc4a41c752a5eb89cf3d1bb690c
Devesh Mathur <78540135+des-igns@users.noreply.github.com>
2023-11-27 17:01:29 +0000

    loop

---
+++ 01_Basics/forloop.js
@@ -0,0 +1,33 @@
+// for (let i = 1; i <=10 ; i++) {
+//     console.log(`Table of ${i}`);
+//     for (let j = 1; j <=10 ; j++) {
+//         console.log(`${i} * ${j}`);
+    
+//     }
+    
+// }
+// let hero = ["batman","superman", "Aquaman"]
+
+// for (let i = 0; i < hero.length; i++) {
+//     const element = hero[i];
+//     console.log(element);
+// }
+
+//break and continue
+// for (let i = 0; i <=20 ; i++) {
+//     if(i==5 ){
+//         console.log("Detected 5");
+//         break;
+//     }
+//     console.log(`${i}`);
+    
+// }
+
+for (let i = 0; i <=10 ; i++) {
+    if(i==5 ){
+        console.log("Detected 5");
+        continue;
+    }
+    console.log(`${i}`);
+    
+}
