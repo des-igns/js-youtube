322cc255a6e90eb630befd6d3bab18380bc32548
Devesh Mathur <78540135+des-igns@users.noreply.github.com>
2023-11-27 06:30:21 +0000

    IIFE

---
+++ 01_Basics/arrow.js
@@ -0,0 +1,37 @@
+// const user = {
+//     name: "Dev",
+//     price: 1234,
+
+//     welcomemsg: function(){
+//         console.log(`${this.name} , is user's name`);
+//     } 
+// }
+
+// console.log(user.welcomemsg())
+
+// const chai = () =>{
+//     let username = "Dev"
+//     console.log(this);
+// }
+// chai()
+
+//Arrow Function
+// const addTwo = (a,b) =>{
+//     return a+b
+// }
+// console.log(addTwo(3,4))
+
+//Implicit Return
+// const addTwo = (a,b) => (a+b)   
+
+
+// IIFE - Immediately Invoked Function Expressions(Used to avoid global scope pollution and run immediately)
+
+// (function chai(){
+//     console.log(`DB Connected`)
+// })();
+
+// ( (name) =>{
+//     console.log(`Name is ${name}`)
+// }
+// )('Devesh');
